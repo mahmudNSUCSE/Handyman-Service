ac85e92e3b7c2537317812a33957dc45fdde0481
Mahmud1431000642 <79445401+mahmudNSUCSE@users.noreply.github.com>
2021-05-23 23:50:34 +0600

    Commit: Implemented PHPUnit unit test

---
+++ Handyman/vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '77f82c7970d2e30df30895e67617488650fc518d',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '77f82c7970d2e30df30895e67617488650fc518d',
+    ),
+    'cr/hashcli' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '6ab58fab13b9d54c94a764dbf3fa6812b75d14d8',
+    ),
+  ),
+);
